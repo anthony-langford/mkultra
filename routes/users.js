b54a1d7007389ed0d01bf86f1539cb40cb297224
Dylan Toyne <dylantoyne@gmail.com>
2017-02-01 21:43:17 +0000

    what

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,6 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
-  // See all users:
-
   router.get("/", (req, res) => {
     knex
       .select("*")
@@ -16,33 +14,5 @@ module.exports = (knex) => {
     });
   });
 
-  router.get("/", (req, res) => {
-    knex
-      .select("*")
-      .from("users")
-      .outerJoin("userMovies", "movies")
-      .then((results) => {
-        res.json(results);
-      });
-  });
-
-  router.post("/api/users", (req, res) => {
-    if (!req.body.text) {
-      console.log("hey");
-      console.log(req.body.text)
-      res.status(400).json({ error: 'Invalid Request: No input in POST body' });
-      return;
-    }
-    const user = req.body.user ? req.body.user : res.status(400).json({ error: 'Invalid User '});
-    const post = {
-      user: user,
-      content: {
-        text: req.body.text
-      },
-    }
-  });
-
   return router;
 }
-
-// Movies, Films, Books, Products:
