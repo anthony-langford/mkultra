14b2ca01cc1f64726377158a3ec93fbaef2321d0
Dylan Toyne <dylantoyne@gmail.com>
2017-02-03 18:48:17 +0000

    updated routes for register/login/logout

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,10 @@
 "use strict";
 
 const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
 const router  = express.Router();
 
 module.exports = (knex) => {
-
-  // router.get("/", (req, res) => {
-  //   knex
-  //     .select("*")
-  //     .from("users")
-  //     .then((results) => {
-  //       res.json(results);
-  //   });
-  // });
 
   router.get("/", (req, res) => {
     knex
@@ -38,6 +30,93 @@ module.exports = (knex) => {
         text: req.body.text
       },
     }
+  });
+
+  router.get("/", (req, res) => {
+    if (req.session.user) {
+      res.render("index", { isLoggedIn: true});
+    } else {
+      res.render("index", { isLoggedIn: false});
+    }
+  });
+
+  router.get("login", (req, res) => {
+    if (req.session.user) {
+      res.redirect("/");
+    } else {
+      res.render("login", { message: req.flash("loginMsg")});
+    }
+  });
+
+  router.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+      if (!email || !password) {
+        req.flash("loginMsg", "Please fillout the required input fields");
+        return res.redirect("login");
+    }
+    knex.select("*")
+    .from("users")
+    .where("email", email)
+    .then((result) => {
+      if (result.length == 0) {
+        req.flash("loginMsg", "No account found: Please sign up");
+        return res.redirect("login");
+      } else if (!bcrypt.compareSync(password, result[0].password)){
+          req.flash("loginMsg", "The password you entered is incorrect. Please try again");
+          return res.redirect("login");
+        } else {
+          return res.redirect('/users');
+        }
+    })
+  });
+
+  router.get("/register", (req, res) => {
+    if (req.session.user) {
+      res.redirect("/");
+    }
+      res.render("register", { message: req.flash("registerMsg")});
+  });
+
+  router.post("/register", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+    if (!email || !password || !name) {
+      req.flash("registerMsg", "Please fill out the required input fields");
+      res.redirect("register");
+    } else {
+      knex.select("*")
+      .from("users")
+      .where("email", email)
+      .then((results) => {
+        if(results.length == 0) {
+          const hashedPassword = bcrypt.hashSync(password);
+          const newUser = {
+            name: name,
+            email: email,
+            password: hashedPassword
+          };
+          knex("users")
+          .insert(newUser)
+          .then(() => {
+            req.session.user = newUser;
+            res.redirect("/");
+          });
+        }
+        else {
+          req.flash("registerMsg", "An account linked to this user currently exists: Please sign in");
+          res.redirect("register");
+        }
+      });
+    }
+  });
+
+  router.get("/logout", (req, res) => {
+    req.session.destroy((err) => {
+      throw err;
+    })
+    res.redirect("/");
   });
 
   return router;
