845f0f31106fddc43a4b1ff5dc2799d6fca54bdf
Dylan Toyne <dylantoyne@gmail.com>
2017-02-03 02:11:40 +0000

    done wit this for the day. It is looking pretty good.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -46,6 +46,14 @@
   $(document).ready(function () {
       window['conclave'] = Conclave;
       Conclave.init();
+
+      $('.forgot-pass').click(function(event) {
+        $(".pr-wrap").toggleClass("show-pass-reset");
+      });
+
+      $('.pass-reset-submit').click(function(event) {
+        $(".pr-wrap").removeClass("show-pass-reset");
+      });
   });
 
   // function submitNewItem() {
