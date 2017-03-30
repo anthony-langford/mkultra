8e7696d282f859baf1cb9261518d2e2473d45033
Anthony Langford <anthony.langford@gmail.com>
2017-03-29 21:48:38 -0400

    fixed db seeds

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,4 +2,21 @@ exports.seed = function(knex, Promise) {
   return Promise.all([
     knex('users').del()
     ])
+    .then(function () {
+      return Promise.all([
+        knex('users').insert({id: 1, name: 'Alice'}),
+        knex('movies').insert({
+          title: 'Gladiator',
+          year: 2000,
+          rating: 8.5,
+          poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTgwMzQzNTQ1Ml5BMl5BanBnXkFtZTgwMDY2NTYxMTE@._V1_SX300.jpg',
+          genres: 'Action, Adventure, Drama',
+          rated: 'R',
+          director: 'Ridley Scott',
+          runtime: 155,
+          plot: "When a Roman general is betrayed and his family murdered by an emperor's corrupt son, he comes to Rome as a gladiator to seek revenge.",
+          date: 1486344897303
+        })
+      ]);
+    })
 };
