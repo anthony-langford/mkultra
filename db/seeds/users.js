52fa09d093ca74339b95a5c0eee9cc68b48ca6c8
Anthony Langford <anthony.langford@gmail.com>
2017-04-03 14:46:28 -0400

    seed users and styling

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -1,23 +1,23 @@
-exports.seed = function(knex, Promise) {
+exports.seed = (knex, Promise) => {
   return Promise.all([
     knex('users').del(),
     knex('movies').del()
-    ])
-    .then(() => {
-      return Promise.all([
-        knex('users').insert({ id: 1, name: 'Alice', email: 'example@eg.com', password: '' }),
-        knex('movies').insert({
-          title: 'Gladiator',
-          year: 2000,
-          rating: 8.5,
-          poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTgwMzQzNTQ1Ml5BMl5BanBnXkFtZTgwMDY2NTYxMTE@._V1_SX300.jpg',
-          genres: 'Action, Adventure, Drama',
-          rated: 'R',
-          director: 'Ridley Scott',
-          runtime: 155,
-          plot: "When a Roman general is betrayed and his family murdered by an emperor's corrupt son, he comes to Rome as a gladiator to seek revenge.",
-          date: 1486344897303
-        })
-      ]);
-    })
+  ])
+  .then(() => {
+    return Promise.all([
+      knex('users').insert({ id: 1, name: 'Alice', email: 'example@eg.com', password: '' }),
+      knex('movies').insert({
+        title: 'Gladiator',
+        year: 2000,
+        rating: 8.5,
+        poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTgwMzQzNTQ1Ml5BMl5BanBnXkFtZTgwMDY2NTYxMTE@._V1_SX300.jpg',
+        genres: 'Action, Adventure, Drama',
+        rated: 'R',
+        director: 'Ridley Scott',
+        runtime: 155,
+        plot: "When a Roman general is betrayed and his family murdered by an emperor's corrupt son, he comes to Rome as a gladiator to seek revenge.",
+        date: 1486344897303
+      })
+    ]);
+  })
 };
